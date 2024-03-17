120eeaab52602cee307bc3ad0f58f525d31ba823
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-03-17 13:12:46 -0300

    melhoria

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": 400,
             "body": "Missing 'cpf' in the request",
-            "headers": {
+            headers: {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": True,
