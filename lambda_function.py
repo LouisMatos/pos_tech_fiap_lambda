58d306ba010f779afe664738e7551ebc7f3fc33c
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-03-17 13:01:22 -0300

    melhoria]

--- lambda_function.py
+++ lambda_function.py
@@ -12,9 +12,9 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
         "body": cpf,
         headers: {
+            "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
         }
