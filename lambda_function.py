5c7cffce1527bb9c72ea4ed8b76d016976cb0378
LouisMatos <luismatos000@gmail.com>
2024-03-17 13:04:23 -0300

    New diff (#7)
    * teste modificado
    
    * melhoria
    
    * melhoria
    
    * melhoria]
    
    ---------
    
    Co-authored-by: Luis Matos <luis.felipe.pereira.matos@gmail.com>

--- lambda_function.py
+++ lambda_function.py
@@ -12,6 +12,10 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": cpf
+        "body": cpf,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
     }
