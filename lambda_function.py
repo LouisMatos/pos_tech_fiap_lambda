0114291a3e4430c61350b3b1574fb3f8191262d9
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2024-03-17 11:55:41 -0300

    teste modificado

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+
     cpf = event['cpf']
+
     logger.info(cpf)
-    return cpf
+
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": cpf
+    }
